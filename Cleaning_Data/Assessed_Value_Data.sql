a0a9b2554f736f2baf090b8c60375df3ae786449
Cbanks02 <cameronbanks545@gmail.com>
2025-07-09 18:49:19 -0400

    Update Assessed_Value_Data.sql

--- Cleaning_Data/Assessed_Value_Data.sql
+++ Cleaning_Data/Assessed_Value_Data.sql
@@ -1,1 +1,27 @@
+-- Dropping prior assessed colomns. 
+ALTER TABLE assessed_values
+DROP COLUMN priland,
+DROP COLUMN pribldg,
+DROP COLUMN pritot;
+
+-- Deleting rows with "tax exempt" under the description column.
+Delete 
+FROM assessed_values
+WHERE description = 'Tax Exempt';
+
+-- Deleting rows with zero values.
+DELETE
+FROM assessed_values
+WHERE aprland = '0' 
+OR aprbldg = '0' 
+OR aprtot = '0'
+OR description = '0';
+
+--Deleting rows with null values.
+DELETE
+FROM assessed_values
+WHERE aprland IS NULL
+OR aprbldg IS NULL
+OR aprtot IS NULL;
+
 
