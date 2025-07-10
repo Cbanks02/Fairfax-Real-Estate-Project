b1a55d0fef9b4f540c9d335902bc24e5f9c00d35
Cbanks02 <cameronbanks545@gmail.com>
2025-07-09 21:00:14 -0400

    Update Appreciation.sql

--- Queries/Appreciation.sql
+++ Queries/Appreciation.sql
@@ -1,1 +1,50 @@
+-- What year did each property size category have the highest annual appreciation compared to their 2025 assessed value?
+
+SELECT
+EXTRACT(YEAR FROM saledt) AS Sale_year,
+ROUND(AVG((aprtot - price )/price) * 100.0, 1) AS avg_appreciation_percent,
+CASE
+	WHEN (sf + (acres * 43560)) >= 100000 THEN 'large'
+	WHEN (sf + (acres * 43560)) >= 50000 THEN 'medium'
+		ELSE 'small'
+END AS property_size_category
+FROM land_data
+JOIN sales_data ON land_data.parid = sales_data.parid
+JOIN assessed_values ON land_data.parid = assessed_values.parid
+Where saledt between '2010-01-01' AND '2015-12-31'
+AND ABS (price - aprtot) < '750000'
+GROUP BY sale_year, property_size_category
+ORDER BY property_size_category DESC;
+
+
+-- What property size category appreciated the most overall?
+
+SELECT
+CASE
+	WHEN (sf + (acres * 43560)) >= 100000 THEN 'large'
+	WHEN (sf + (acres * 43560)) >= 50000 THEN 'medium'
+		ELSE 'small'
+END AS property_size_category,
+ROUND(AVG((aprtot - price )/price) * 100.0, 1) AS appreciation_percent
+FROM land_data
+JOIN sales_data ON land_data.parid = sales_data.parid
+JOIN assessed_values ON land_data.parid = assessed_values.parid
+WHERE saledt BETWEEN '2010-01-01' AND '2015-12-31'
+AND ABS (price - aprtot) < '750000'
+GROUP BY property_size_category
+ORDER BY appreciation_percent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
