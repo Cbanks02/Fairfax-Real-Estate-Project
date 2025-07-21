7180c505d69a5492e960d0a4dad6d10c2ad3b86e
Cbanks02 <cameronbanks545@gmail.com>
2025-07-21 00:34:37 -0400

    Update Price_Gap.sql

--- Queries/Price_Gap.sql
+++ Queries/Price_Gap.sql
@@ -1,5 +1,6 @@
 -- I added additional filters to all queries to exclude potential outliers that could skew the data, including price gaps exceeding $750,000 or below -$750,000, 
--- Due to minimal sample size, non valid sales, or missing data, the sales between years between 2016 - 2025 were not inlcuded in calculations
+-- and total property (land and building) square footage exceeding 40,000. Due to minimal sample size, non valid sales, or missing data,
+-- the sales between years between 2016 - 2025 were not inlcuded in calculations
 
 -- which year has the biggest price gaps?
 
