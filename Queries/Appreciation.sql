cfa1bdbfed78b6d013758960344d7befb0cbbdcd
Cbanks02 <cameronbanks545@gmail.com>
2025-07-20 21:03:07 -0400

    Update Appreciation.sql

--- Queries/Appreciation.sql
+++ Queries/Appreciation.sql
@@ -12,6 +12,7 @@ FROM land_data
 JOIN sales_data ON land_data.parid = sales_data.parid
 JOIN assessed_values ON land_data.parid = assessed_values.parid
 Where saledt between '2010-01-01' AND '2015-12-31'
+AND (sf + (acres * 43660)) < 40000
 AND ABS (price - aprtot) < '750000'
 GROUP BY sale_year, property_size_category
 ORDER BY property_size_category DESC;
@@ -30,6 +31,7 @@ FROM land_data
 JOIN sales_data ON land_data.parid = sales_data.parid
 JOIN assessed_values ON land_data.parid = assessed_values.parid
 WHERE saledt BETWEEN '2010-01-01' AND '2015-12-31'
+AND (sf + (acres * 43660)) < 40000
 AND ABS (price - aprtot) < '750000'
 GROUP BY property_size_category
 ORDER BY appreciation_percent;
