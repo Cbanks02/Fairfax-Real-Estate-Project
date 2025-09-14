27ce95eebf05917afedbb91c34c30a666a3a317b
Cbanks02 <cameronbanks545@gmail.com>
2025-09-13 20:41:31 -0400

    Update Assessed_Value_Data.sql

--- Cleaning_Data/Assessed_Value_Data.sql
+++ Cleaning_Data/Assessed_Value_Data.sql
@@ -1,13 +1,19 @@
--- Dropping prior assessed colomns.  
+--For this personal project, I deleted rows for simplicity. 
+--In a professional setting, I would preserve raw data and filter instead. 
+
+
+-- Dropping prior assessed colomns. 
 ALTER TABLE assessed_values
 DROP COLUMN priland,
 DROP COLUMN pribldg,
 DROP COLUMN pritot;
 
+
 -- Deleting rows with "tax exempt" under the description column.
 Delete 
 FROM assessed_values
 WHERE description = 'Tax Exempt';
+
 
 -- Deleting rows with zero values.
 DELETE
@@ -17,6 +23,7 @@ OR aprbldg = '0'
 OR aprtot = '0'
 OR description = '0';
 
+
 --Deleting rows with null values.
 DELETE
 FROM assessed_values
@@ -24,4 +31,9 @@ WHERE aprland IS NULL
 OR aprbldg IS NULL
 OR aprtot IS NULL;
 
+
+--Deleting appraisal totals over $800,000.
+DELETE 
+FROM assessed_values 
+WHERE aprtot > '800000'
 
