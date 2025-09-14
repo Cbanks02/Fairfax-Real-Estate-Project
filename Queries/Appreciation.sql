38bdf181e279ba504385775578ca88a7945b1d6a
Cbanks02 <cameronbanks545@gmail.com>
2025-09-13 21:06:58 -0400

    Update Appreciation.sql

--- Queries/Appreciation.sql
+++ Queries/Appreciation.sql
@@ -2,6 +2,20 @@
 -- and total property (land and building) square footage exceeding 40,000. Due to minimal sample size, non valid sales, or missing data,
 -- the sales between years between 2016 - 2025 were not inlcuded in calculations
 
+---------------------------------------------------------------------------------
+-- Joining sales, land, and assessed values into one dataset
+
+CREATE TABLE fairfax_property_data AS
+SELECT 
+sales_data.parid, sales_data.saledt, sales_data.price,
+land_data.sf, land_data.acres,
+assessed_values.aprland, assessed_values.aprbldg, assessed_values.aprtot,
+sales_data.saleval_desc, land_data.code_desc, assessed_values.description
+FROM sales_data
+JOIN land_data ON sales_data.parid = land_data.parid
+JOIN assessed_values ON sales_data.parid = assessed_values.parid;
+---------------------------------------------------------------------------------
+
 -- What year did each property size category have the highest annual appreciation compared to their 2025 assessed value?
 
 SELECT
