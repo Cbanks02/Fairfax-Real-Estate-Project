8bcfa875503fb275801c9ed0cb66cd92ce963389
Cbanks02 <cameronbanks545@gmail.com>
2025-09-13 20:37:31 -0400

    Update Sales_Data.sql

--- Cleaning_Data/Sales_Data.sql
+++ Cleaning_Data/Sales_Data.sql
@@ -1,30 +1,37 @@
+--For this personal project, I deleted rows for simplicity. 
+--In a professional setting, I would preserve raw data and filter instead. 
+
+
 -- Removing zero and null values from the price column.
 DELETE
 FROM sales_data
 WHERE price = '0'
 OR price IS NULL;
 
--- Removing null values and those that do not contain the word 'verified' in the sales validation description column.
+
+-- Removing null values and those that do not contain the word 'verified'
+-- in the sales validation description column.
 DELETE
 FROM sales_data
 WHERE saleval_desc IS NULL 
 OR LOWER (saleval_desc) NOT LIKE '%verified%';
 
---Removing duplicates from the sales data table.
+
+--Removing duplicate parcel IDs and keeping the most recent.  
 DELETE
 FROM sales_data
-WHERE (parid, saledt, price, saleval_desc) IN 
-(SELECT parid, saledt, price, saleval_desc
-FROM sales_data
-GROUP BY parid, saledt, price, saleval_desc
-HAVING COUNT (*) > 1);
+WHERE saledt BETWEEN '2010-01-01' AND '2015-12-31' 
+AND saledt < (SELECT MAX (sales_data2.saledt)
+FROM sales_data AS sales_data2
+WHERE (sales_data2.parid) = sales_data.parid 
+AND sales_data2.saledt BETWEEN '2010-01-01' AND '2015-12-31'
+);
+
 
 --Removing additional non-verified rows.
 DELETE 
 FROM sales_data
-WHERE saleval_desc = 'Atypical price - could not be verified';
-
-
-
-
-
+WHERE saleval_desc IN(
+'Atypical price - could not be verified',
+'Valid and verified multi-parcel sale'
+);
