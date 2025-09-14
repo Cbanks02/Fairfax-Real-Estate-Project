efba2680e1f7240bb8f73ca088b098827f25bbcd
Cbanks02 <cameronbanks545@gmail.com>
2025-09-13 20:39:50 -0400

    Update Land_Data.sql

--- Cleaning_Data/Land_Data.sql
+++ Cleaning_Data/Land_Data.sql
@@ -1,3 +1,7 @@
+--For this personal project, I deleted rows for simplicity. 
+--In a professional setting, I would preserve raw data and filter instead. 
+
+
 -- Removing zero values from the square footage and acres columns.
 DELETE 
 FROM land_data
@@ -11,16 +15,6 @@ FROM land_data
 WHERE sf IS NULL 
 AND acres IS NULL
 AND code_desc IS NULL;
-
-
--- Removing duplicate rows.
-DELETE
-FROM land_data
-WHERE (parid, sf, acres, code_desc) IN 
-(SELECT parid, sf, acres, code_desc
-FROM land_data
-GROUP BY parid, sf, acres, code_desc
-HAVING COUNT (*) > 1);
 
 
 -- Removing values from the code description column that are not relevant to the analysis.
@@ -38,18 +32,22 @@ WHERE Code_desc NOT IN (
 
 -- Filling in missing values in the Square foot column.
 UPDATE land_data
-SET sf = ROUND(acres * 43560.0, 0)
-WHERE sf = '0' AND acres > '0';
+SET sf = ROUND(acres * 43560, 0)
+WHERE sf = 0 AND acres > 0;
 
 
 -- Filling in missing values in the acres column.
 UPDATE land_data
-SET acres = sf / 43560.0
-WHERE acres = '0' AND sf > '0';
+SET acres = sf / 43560
+WHERE acres = 0 AND sf > 0;
 
 
 --Rounding the values in the acres column to four decimal places.
 UPDATE land_data
-SET acres = round(acres,4);
+SET acres = round(acres,2);
 
 
+-- Deleting rows where total square feet (property+Land) is over 40,000.
+DELETE
+FROM land_data
+WHERE (sf + (acres * 43560)) > 40000 
